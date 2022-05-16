1bc82709da9045696a4603bc6feeb0bce3143297
Safiullah Omar <omar.safiullah@gmail.com>
2022-05-16 08:27:24 +0430

    final validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,14 @@ export class AppComponent {
 
   constructor (private formBuilder :FormBuilder){}
   ngOnInit(){
+
     this.registerForm=this.formBuilder.group({
       firstName:['',Validators.required],
       lastName:['',Validators.required],
-      Email:['',Validators.required],
-      Password:['',Validators.required]
+      Email:['',[Validators.required,Validators.email]],
+      Password:['',[Validators.required,Validators.min(4)]]
     });
+
   }
 
   onSubmit(){
